46c649c36a334c981d899c3cb19bb6083e13d39f
prokopenya <igorprokopenya@gmail.com>
2020-01-26 22:13:49 +0300

    add configserver.v2

--- counterservice/src/main/java/com/springboot_microservices/counterservice/api/CounterController.java
+++ counterservice/src/main/java/com/springboot_microservices/counterservice/api/CounterController.java
@@ -1,18 +1,23 @@
 package com.springboot_microservices.counterservice.api;
 
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RefreshScope
 public class CounterController {
 
     private static AtomicLong count = new AtomicLong(0);
 
+    @Value(value = "${counter.prefixMessage}")
+    private String prefixMessage;
+
     @GetMapping(path = "/count")
-    public long getCount(){
-        return count.getAndIncrement();
+    public String getCount(){
+        return prefixMessage + count.getAndIncrement();
     }
 }
