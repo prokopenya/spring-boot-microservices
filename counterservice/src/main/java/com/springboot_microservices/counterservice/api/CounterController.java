b1d2687eeb83f73074b3aa2266bc477225038740
prokopenya <igorprokopenya@gmail.com>
2020-01-26 13:51:36 +0300

    create countservice

---
+++ counterservice/src/main/java/com/springboot_microservices/counterservice/api/CounterController.java
@@ -0,0 +1,18 @@
+package api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class CounterController {
+
+    private static AtomicLong count = new AtomicLong(0);
+
+    @GetMapping(path = "/count")
+    public long getCount(){
+        return count.getAndIncrement();
+    }
+}
